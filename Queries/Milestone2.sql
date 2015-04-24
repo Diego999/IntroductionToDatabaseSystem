5f4cd733c4bc82f0ff472619a8fd5390f7594c07
Diego999 <diegoantognini@gmail.com>
2015-04-24 15:36:51 +0200

    Optimize queries

--- Queries/Milestone2.sql
+++ Queries/Milestone2.sql
@@ -1,13 +1,13 @@
-#Compute the number of movies per year. Make sure to include tv and video movies.
-# ~2.2 sec on Macbook Pro mid-2010
+#a Compute the number of movies per year. Make sure to include tv and video movies.
+# ~2.0 sec on Macbook Pro mid-2010
 # ~1.5 sec on Macbook Pro late-2013
 SELECT P.year, COUNT(P.id)
-FROM `production` P, `singleproduction` S
-WHERE P.id = S.id AND S.kind_id IN (SELECT K.id FROM `kind` K WHERE K.name IN ("tv movie","video movie","movie"))
+FROM `production` P INNER JOIN `singleproduction` S ON P.id = S.id 
+WHERE S.kind_id IN (SELECT K.id FROM `kind` K WHERE K.name IN ("tv movie","video movie","movie"))
 GROUP BY P.year;
 
-#Compute the ten countries with most production companies
-# ~18 sec on Macbook Pro mid-2010
+#b Compute the ten countries with most production companies
+# ~11 sec on Macbook Pro mid-2010
 # ~5 sec on Macbook Pro late-2013
 SELECT COU.`id`, COU.`code`, SUB.`number`
 FROM
@@ -19,12 +19,9 @@ FROM
 				(
 					SELECT PC.`company_id`
 					FROM `productioncompany` PC
-					WHERE PC.`type_id` =
-						(
-							SELECT TY.`id`
-							FROM `type` TY
-							WHERE TY.`name`="production companies"
-						)
+					INNER JOIN `type` TY ON PC.`type_id` = TY.`id`
+					WHERE TY.`name`= "production companies"
+						
 				) PC
                 ON PC.`company_id` = COM.`id`
 		GROUP BY COM.`country_id`
@@ -34,60 +31,58 @@ FROM
 	) SUB
     INNER JOIN `country` COU ON SUB.`country_id` = COU.`id`;
 
-#Compute the min, max and average career duration. (A career length is implied by the first and last production of a person)
+#c Compute the min, max and average career duration. (A career length is implied by the first and last production of a person)
 # ~835 on Macbook Pro mid-2010
 # ~447 on Macbook Pro late-2013
 SELECT MIN(T.careerDuration) AS min, MAX(T.careerDuration) AS max, AVG(T.careerDuration) AS avg
-FROM(
+FROM (
     SELECT (MAX(P.year) - MIN(P.year)) as careerDuration
-	FROM 
-    (SELECT DISTINCT C.person_id, C.production_id FROM `casting` C) C, 
-    (SELECT P.id, P.year FROM `production` P WHERE P.year IS NOT NULL) P
-	WHERE C.production_id = P.id
+	FROM `casting` C INNER JOIN `production` P
+	ON C.production_id = P.id
+    WHERE P.year IS NOT NULL
 	GROUP BY C.person_id
     ) T;
-
-#Compute the min, max and average number of actors in a production
-# ~195 sec on Macbook Pro 2010
+    
+#d Compute the min, max and average number of actors in a production
+# ~175 sec on Macbook Pro 2010
 # ~95 sec on Macbook Pro late-2013
 SELECT MIN(T.number) as min, MAX(T.number) as max, AVG(T.number) as avg
 FROM(
-	SELECT COUNT(id) as number
-	FROM `casting` C
-	WHERE C.role_id = (SELECT R.id FROM `role` R WHERE R.name = "actor")
+	SELECT COUNT(C.id) as number
+	FROM `casting` C INNER JOIN `role` R ON C.role_id = R.id
+    WHERE R.name = "actor"
 	GROUP BY C.production_id
     ) T;
 
-#Compute the min, max and average height of female persons.    
-# ~2.6 on Macbook Pro mid-2010
+#e Compute the min, max and average height of female persons.    
+# ~1.0 on Macbook Pro mid-2010
 # ~2.5 on Macbook Pro late-2013
 SELECT MIN(P.height) AS min, MAX(P.height) AS max, AVG(P.height) AS avg
 FROM `person` P
 WHERE P.height IS NOT NULL AND P.gender = "f";
 
-#List all pairs of persons and movies where the person has both directed the movie and acted in the movie.
+#f List all pairs of persons and movies where the person has both directed the movie and acted in the movie.
 #Do not include tv and video movies.
-# ~135 sec on Macbook Pro mid-2010
+# ~215 sec on Macbook Pro mid-2010
 # ~324 sec on Macbook Pro late-2013
 SELECT DISTINCT C1.person_id, C1.production_id
 FROM 
 	(SELECT C.person_id, C.production_id
-	FROM `casting` C
-	WHERE C.role_id = (SELECT R.id FROM `role` R WHERE R.name = "actor")
-	AND C.production_id NOT IN (SELECT S.id
-								FROM `singleproduction` S
-								WHERE S.kind_id IN (SELECT K.id FROM `kind` K WHERE K.name = "movie")
-								)) C1,
-	(SELECT C.person_id
-	FROM `casting` C
-	WHERE C.role_id = (SELECT R.id FROM `role` R WHERE R.name = "producer")
-	AND C.production_id NOT IN (SELECT S.id
-								FROM `singleproduction` S
-								WHERE S.kind_id IN (SELECT K.id FROM `kind` K WHERE K.name = "movie")
-								)) C2
+	FROM `casting` C INNER JOIN `role` R ON C.role_id = R.id 
+    WHERE R.name = "actor"
+	AND C.production_id NOT IN (
+								SELECT S.id	FROM `singleproduction` S INNER JOIN `kind` K ON S.kind_id = K.id WHERE K.name = "movie"
+                                )) C1 
+	,
+	(SELECT C.person_id, C.production_id
+	FROM `casting` C INNER JOIN `role` R ON C.role_id = R.id 
+    WHERE R.name = "producer"
+	AND C.production_id NOT IN (
+								SELECT S.id	FROM `singleproduction` S INNER JOIN `kind` K ON S.kind_id = K.id WHERE K.name = "movie"
+                                )) C2
 WHERE C1.person_id = C2.person_id;
 
-#List the three most popular character names
+#g List the three most popular character names
 # 11.3 sec on Macbook Pro mid-2010
 # 18 sec on Macbook Pro late-2013
 SELECT Ch.name
@@ -98,5 +93,5 @@ FROM (
 	GROUP BY Ca.character_id
     ORDER BY number DESC
     LIMIT 0,3
-    ) T, `character` Ch
-WHERE T.character_id = Ch.id;
+    ) T INNER JOIN `character` Ch
+ON T.character_id = Ch.id;
