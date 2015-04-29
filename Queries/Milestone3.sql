57ea1dc4fed87ee020574e7f77d5af2977706f39
Diego999 <diegoantognini@gmail.com>
2015-04-29 17:43:19 +0200

    Add skeleton queries milestones 3

---
+++ Queries/Milestone3.sql
@@ -0,0 +1,60 @@
+#a Find the actors and actresses (and report the productions) who played in a production
+#where they were 55 or more year older than the youngest actor/actress playing
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#b Given an actor, compute his most productive year
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#c Given a year, list the company with the highest number of productions in each genre
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#d Compute who worked with spouses/children/potential relatives on the same production.
+#(You can assume that the same real surname implies a relation)
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#e Compute the of average number of actors per production per year
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#f Compute the average number of episodes per season
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#g Compute the average number of seasons per series
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#h Compute the top ten tv-series (by number of seasons)
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#i Compute the top ten tv-series (by number of episodes per season)
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#j Find actors, actresses and directors who have movies (including tv movies and video movies) released
+#after their death
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#k For each year, show three companies that released the most movies
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#l List all living people who are opera singers ordered from youngest to oldest
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#m List 10 most ambiguous credits (pairs of people and productions) ordered by the degree of ambiguity.
+#A credit is ambiguous if either a person has multiple alternative names or a production has multiple alternative titles.
+#The degree of ambiguity is a product of the number of possible names (real name + all alternatives) and the number of possible titles (real + alternatives)
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
+
+#n For each country, list the most frequent character name that appears in the productions of a production company (not a distributor) from that country
+# ~X sec on Macbook Pro mid-2010
+# ~X sec on Macbook Pro late-2013
